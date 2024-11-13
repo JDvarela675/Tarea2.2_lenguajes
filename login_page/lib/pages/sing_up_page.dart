b5f994025d329f104f817f71a3ee223f76727538
kishaMM <kishammejia@gmail.com>
2024-11-13 15:17:18 -0600

    diseño de sign up/registro

--- login_page/lib/pages/sing_up_page.dart
+++ login_page/lib/pages/sing_up_page.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:login_page/widgets/caja_texto.dart';
+
 
 class SingUpPage extends StatelessWidget {
-  const SingUpPage({super.key});
+  SingUpPage({super.key});
+
+  final userController = TextEditingController(text: 'jdvarelas@unah.hn');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          }, icon: Icon(Icons.arrow_back,
+          size: 30,
+          color: Colors.white,)
+          ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          height: MediaQuery.of(context).size.height - 40,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Text("Registrate", style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  SizedBox(height: 30,),
+                  Text("¡Crea tu cuenta con nosotros!",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey[300],
+                    ),)
+                ],
+              ),
+              Column(
+                children: [
+                  CustomTextBox(Title: "Nombre", UserController: TextEditingController()),
+                  SizedBox(height: 15),
+                  CustomTextBox(Title: "Email", UserController: TextEditingController()),
+                  SizedBox(height: 15),
+                  CustomTextBox(Title: "Contraseña", UserController: TextEditingController()),
+                  SizedBox(height: 15),
+                  CustomTextBox(Title: "Confirma tu contraseña", UserController: TextEditingController())
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 4, left: 4),
+                decoration: 
+                BoxDecoration(
+                  borderRadius: BorderRadius.circular(40),
+                  border: Border(
+                    bottom: BorderSide(color: Colors.white),
+                    top: BorderSide(color: Colors.white),
+                    left: BorderSide(color: Colors.white),
+                    right: BorderSide(color: Colors.white),
+                  )
+                ),
+
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
