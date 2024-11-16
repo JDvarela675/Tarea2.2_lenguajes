0057d3f10b6a8ab54764702cdee3d1a433c7d939
kishaMM <kisha.mejia@unah.hn>
2024-11-15 20:38:36 -0600

    controllers

--- login_page/lib/pages/sing_up_page.dart
+++ login_page/lib/pages/sing_up_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:login_page/widgets/caja_texto.dart';
 
-
 class SingUpPage extends StatelessWidget {
   SingUpPage({super.key});
 
-  final userController = TextEditingController(text: 'jdvarelas@unah.hn');
+  final emailController = TextEditingController();
+  final nombreController = TextEditingController();
+  final contraseniaController = TextEditingController();
+  final verificacionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -17,75 +19,50 @@ class SingUpPage extends StatelessWidget {
         leading: IconButton(
           onPressed: (){
             Navigator.pop(context);
-          }, icon: Icon(Icons.arrow_back,
+          }, icon: const Icon(Icons.arrow_back,
           size: 30,
           color: Colors.white,)
           ),
       ),
+      
       body: SingleChildScrollView(
+         
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 30),
-          height: MediaQuery.of(context).size.height - 40,
-          width: double.infinity,
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Column(
                 children: [
-                  Text("Registrate", style: TextStyle(
+                  const Text("Registrate", style: TextStyle(
                     color: Colors.cyan,
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                   ),),
-                  SizedBox(height: 30,),
+                  const SizedBox(height: 30,),
                   Text("¡Crea tu cuenta con nosotros!",
                     style: TextStyle(
                       fontSize: 20,
                       color: Colors.grey[300],
-                    ),)
+                    ),),
+                  const SizedBox(height: 30), 
                 ],
               ),
               Column(
                 children: [
-                  CustomTextBox(Title: "Nombre", UserController: TextEditingController()),
+                  CustomTextBox(Title: "Nombre", UserController: nombreController),
                   SizedBox(height: 15),
-                  CustomTextBox(Title: "Email", UserController: TextEditingController()),
+                  CustomTextBox(Title: "Email", UserController: emailController),
                   SizedBox(height: 15),
-                  CustomTextBox(Title: "Contraseña", UserController: TextEditingController(), keyboardType: TextInputType.visiblePassword,),
+                  CustomTextBox(Title: "Contraseña", UserController: contraseniaController, keyboardType: TextInputType.visiblePassword,),
                   SizedBox(height: 15),
-                  CustomTextBox(Title: "Confirma tu contraseña", UserController: TextEditingController(), keyboardType: TextInputType.visiblePassword,)
+                  CustomTextBox(Title: "Confirma tu contraseña", UserController: verificacionController, keyboardType: TextInputType.visiblePassword,)
                 ],
-
               ),
+              
 
-
-             FloatingActionButton.extended(onPressed: (){},
-             label: const Text('Sig in',
-             style: TextStyle(color: Colors.white),
-             ),
-             backgroundColor: Colors.cyan,
-             
-            shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(80),
-            ),
-
-             extendedPadding: EdgeInsets.all(55), //hace mas grande el boton
-             ),
-
-              Container(
-                padding: EdgeInsets.only(top: 4, left: 4),
-                decoration: 
-                BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  border: Border(
-                    bottom: BorderSide(color: Colors.white),
-                    top: BorderSide(color: Colors.white),
-                    left: BorderSide(color: Colors.white),
-                    right: BorderSide(color: Colors.white),
-                  )
-                ),
-
-              )
+              
             ],
           ),
         ),
